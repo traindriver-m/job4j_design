963c507636db97d1ced73d162a8884abeb3b04bc
Traindriver-M <sergioparovozov@mail.ru>
2021-11-04 20:55:51 +0300

    3. Переопределить только hashCode [#1003 #224584]

--- src/main/java/ru/job4j/map/Main.java
+++ src/main/java/ru/job4j/map/Main.java
@@ -1,15 +1,17 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.Calendar.*;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Calendar birthday = new GregorianCalendar(1976, Calendar.FEBRUARY, 18);
+        Calendar birthday = new GregorianCalendar(1976, Calendar.FEBRUARY, 18, 19, 20);
+        Calendar birthday2 = new GregorianCalendar(1976, Calendar.FEBRUARY, 18, 19, 20);
         User user1 = new User("Sergey", 2, birthday);
-        User user2 = new User("Sergey", 2, birthday);
+        User user2 = new User("Sergey", 2, birthday2);
         Map<User, Object> map = new HashMap<>();
         map.put(user1, new Object());
         map.put(user2, new Object());
--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -12,4 +12,14 @@ public class User {
         this.children = children;
         this.birthday = birthday;
     }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + children;
+        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
+        return result;
+    }
+
+
 }
