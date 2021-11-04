6bf0d6b45820443773970d9c33230ddd9c7e86bf
Traindriver-M <sergioparovozov@mail.ru>
2021-11-04 20:32:06 +0300

    2. Без переопределения equals и hashCode [#1005 #224576]

---
+++ src/main/java/ru/job4j/map/Main.java
@@ -0,0 +1,18 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Calendar birthday = new GregorianCalendar(1976, Calendar.FEBRUARY, 18);
+        User user1 = new User("Sergey", 2, birthday);
+        User user2 = new User("Sergey", 2, birthday);
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println(map);
+    }
+}
