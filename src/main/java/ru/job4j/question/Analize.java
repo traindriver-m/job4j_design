3c3e2881f9ba8acd90f50a00f1e77e440f80a88a
Traindriver-M <sergioparovozov@mail.ru>
2021-11-14 17:48:50 +0300

    2. Статистика по коллекции. [#45889 #227958]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -1,6 +1,5 @@
 package ru.job4j.question;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -8,24 +7,22 @@ import java.util.stream.Collectors;
 public class Analize {
 
     public static Info diff(Set<User> previous, Set<User> current) {
-        int added = 0;
+        int added;
         int changed = 0;
         int deleted = 0;
-        if (!current.equals(previous)) {
-            Map<Integer, String> map = current.stream()
+            Map<Integer, String> currentMap = current.stream()
                     .collect(Collectors.toMap(User::getId, User::getName));
-            for (User u : previous) {
-                int id = u.getId();
-                String name = u.getName();
-                String retValue = map.putIfAbsent(id, name);
-                if (retValue == null) {
+            for (User user : previous) {
+                int id = user.getId();
+                String name = user.getName();
+                String returnValue = currentMap.putIfAbsent(id, name);
+                if (returnValue == null) {
                     deleted++;
-                } else if (!retValue.equals(name)) {
+                } else if (!returnValue.equals(name)) {
                     changed++;
                 }
             }
-            added = map.size() - previous.size();
-        }
+            added = currentMap.size() - previous.size();
         return new Info(added, changed, deleted);
     }
 }
