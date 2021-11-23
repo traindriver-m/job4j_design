e9e084c6b59729c19c91b5c2b52c6c172f1e092a
Traindriver-M <sergioparovozov@mail.ru>
2021-11-23 19:14:26 +0300

    4.2. Поиск дубликатов [#315066 #231417]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -6,6 +6,10 @@ import java.nio.file.Path;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("./"), new DuplicatesVisitor());
+        DuplicatesVisitor visitor = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("./"), visitor);
+        visitor.resultMap.entrySet().stream()
+                .flatMap(s -> s.getValue().stream())
+                .forEach(System.out::println);
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -6,24 +6,26 @@ import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    Map<FileProperty, Path> filePropertyMap = new HashMap<>();
-    boolean mark = false;
+    Map<FileProperty, HashSet<Path>> allFilePropertyMap = new HashMap<>();
+    Map<FileProperty, HashSet<Path>> resultMap = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty fileProperty = new FileProperty(file.toFile().length(), file.toFile().getName());
-
-        if (filePropertyMap.containsKey(fileProperty)) {
-            if (!filePropertyMap.get(fileProperty).equals(file.toAbsolutePath()) && !mark) {
-                System.out.println(filePropertyMap.get(fileProperty));
-                mark = true;
-            }
-            System.out.println(file.toAbsolutePath());
+        HashSet<Path> pathSet = new HashSet<>();
+        if (allFilePropertyMap.containsKey(fileProperty)) {
+            pathSet = allFilePropertyMap.get(fileProperty);
+            pathSet.add(file.toAbsolutePath());
+            allFilePropertyMap.put(fileProperty, pathSet);
+            resultMap.put(fileProperty, pathSet);
+        } else {
+            pathSet.add(file.toAbsolutePath());
+            allFilePropertyMap.put(fileProperty, pathSet);
         }
-        filePropertyMap.put(fileProperty, file.toAbsolutePath());
         return super.visitFile(file, attrs);
     }
 }
