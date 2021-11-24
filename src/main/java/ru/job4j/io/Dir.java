2238819ee8dda51f10abbb88aa640c4762779670
Traindriver-M <sergioparovozov@mail.ru>
2021-11-24 16:09:30 +0300

    Revert "4.0. File [#252491]"
    This reverts commit a914414f

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -4,25 +4,15 @@ import java.io.File;
 
 public class Dir {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
-        } else if (args.length != 2) {
-            throw new IllegalArgumentException("Invalid number of parameters. "
-                    + "Set two parameters to run the program.");
-        }
-        File file = new File(args[0]);
-        String extension = args[1];
-
+        File file = new File("E:\\projects");
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        for (File subfile : file.listFiles()) {
-            if (subfile.getName().endsWith(extension)) {
-                System.out.println(subfile.getName() + " (" + subfile.length() + " bytes).");
-            }
+        for (File subfile : Objects.requireNonNull(file.listFiles())) {
+            System.out.println(subfile.getName() + " " + subfile.length());
         }
     }
 }
