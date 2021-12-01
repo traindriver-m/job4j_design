a29691aae2903e4e732502fa5fc85aa4c017c282
Traindriver-M <sergioparovozov@mail.ru>
2021-12-01 22:05:39 +0300

    7. Scanner [#504791 #233536]

---
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -0,0 +1,76 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.*;
+
+public class CSVReader {
+
+    public static void handle(ArgsName argsName) {
+        int count = 0;
+        StringBuilder result = new StringBuilder();
+        try (var scanner = new Scanner(new FileReader(ArgsName.getValues().get("path")))) {
+            List<Integer> indexList = new ArrayList<>();
+            String[] filters = ArgsName.getValues().get("filter").split(",");
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] strings = line.split(ArgsName.getValues().get("delimiter"));
+                List<String> stringList = Arrays.asList(strings);
+                for (String filter : filters) {
+                    if (stringList.contains(filter)) {
+                        indexList.add(stringList.indexOf(filter));
+                    }
+                }
+                for (Integer index : indexList) {
+                    result.append(stringList.get(index));
+                    count++;
+                    if (count < indexList.size()) {
+                        result.append(";");
+                    }
+                }
+                if (scanner.hasNext()) {
+                    result.append(System.lineSeparator());
+                    count = 0;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if ("stdout".equals(ArgsName.getValues().get("out"))) {
+            System.out.println(result);
+        } else {
+            outputResult(result);
+        }
+    }
+
+    public static void validationKeys(Map<String, String> values) {
+        if (values.size() != 4 || !values.containsKey("path") || !values.containsKey("delimiter")
+                || !values.containsKey("out") || !values.containsKey("filter")) {
+            throw new IllegalArgumentException("Incorrect program launch parameters.");
+        }
+    }
+
+    private static void checkFile(Map<String, String> values) {
+        File file = new File(values.get("path"));
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("The file does not exist %s", file.getAbsoluteFile()));
+        }
+    }
+
+    private static void outputResult(StringBuilder result) {
+        try (PrintWriter printWriter = new PrintWriter(new FileWriter(ArgsName.getValues().get("out"),
+                Charset.forName("WINDOWS-1251"), true))) {
+            printWriter.println(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ArgsName argsName = ArgsName.of(args);
+        Map<String, String> values = ArgsName.getValues();
+        validationKeys(values);
+        checkFile(values);
+        handle(argsName);
+    }
+}
