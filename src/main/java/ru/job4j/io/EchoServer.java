5d5cec2d392af0e5ded1f7f000c66baa55c1a793
Traindriver-M <sergioparovozov@mail.ru>
2021-12-02 19:13:15 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -9,20 +9,30 @@ import java.net.Socket;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
+        String outMsg = "";
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         String[] strings = str.split("=");
-                        if (strings.length > 1 && strings[1].startsWith("Bye")) {
-                            server.close();
+                        String msg = "";
+                        if (strings.length > 1) {
+                            msg = strings[1];
                         }
-                        System.out.println(str);
+                        if (msg.startsWith("Exit ")) {
+                            server.close();
+                        } else if (msg.startsWith("Hello ")) {
+                            outMsg = "Hello";
+                            break;
+                        } else {
+                            outMsg = "What?";
+                        }
                     }
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(outMsg.getBytes());
                     out.flush();
                 }
             }
