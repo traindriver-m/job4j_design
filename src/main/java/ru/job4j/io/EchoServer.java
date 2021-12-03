0762fb8ec9ecae0247f49ec29c7cda66264c37a4
Traindriver-M <sergioparovozov@mail.ru>
2021-12-03 23:39:50 +0300

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,30 +1,47 @@
 package ru.job4j.io;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
+        String outMsg = "";
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.contains("?msg=Bye")) {
-                            server.close();
+                        String[] strings = str.split("=");
+                        String msg = "";
+                        if (strings.length > 1) {
+                            msg = strings[1];
                         }
-                        System.out.println(str);
+                        if (msg.startsWith("Exit ")) {
+                            server.close();
+                        } else if (msg.startsWith("Hello ")) {
+                            outMsg = "Hello";
+                            break;
+                        } else {
+                            outMsg = "What?";
+                        }
                     }
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(outMsg.getBytes());
                     out.flush();
                 }
             }
+        } catch (Exception e) {
+            LOG.error("Exception", e);
         }
     }
 }
