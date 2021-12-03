113d9e01605f73e16a35f73b898d8a331e1c6a9e
Traindriver-M <sergioparovozov@mail.ru>
2021-12-03 23:36:22 +0300

    0. Что такое Socket? [#4850 #235176]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,8 +1,5 @@
 package ru.job4j.io;
 
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,38 +8,23 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
-
-    public static void main(String[] args) {
-        String outMsg = "";
+    public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        String[] strings = str.split("=");
-                        String msg = "";
-                        if (strings.length > 1) {
-                            msg = strings[1];
-                        }
-                        if (msg.startsWith("Exit ")) {
-                            server.close();
-                        } else if (msg.startsWith("Hello ")) {
-                            outMsg = "Hello";
-                            break;
-                        } else {
-                            outMsg = "What?";
-                        }
-                    }
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    out.write(outMsg.getBytes());
+                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
+                        if (str.contains("?msg=Bye")) {
+                            server.close();
+                        }
+                        System.out.println(str);
+                    }
                     out.flush();
                 }
             }
-        } catch (Exception e) {
-            LOG.error("Exception", e);
         }
     }
 }
